d4645bcdb1028db9ea90b92636f7f240c96be6a3
Aashutosh Singh Baghel <aashu@alpine.my.domain>
2025-09-15 15:14:30 +0530

    oh yeah greedy

--- SEM3/CS302/Lab_Work/16-greedy_knapsack_problem.c
+++ SEM3/CS302/Lab_Work/16-greedy_knapsack_problem.c
@@ -2,6 +2,7 @@
 
 #define SIZE 10
 #define DE ;
+#define TOTE_SIZE 200
 
 /*
 void swap(int *a, int*b){
@@ -42,7 +43,7 @@ void sort_array(float arr[], int values[], int weights[], int n){
 				temp_weight = weights[j];
 				weights[j] = weights[j + 1];
 				weights[j + 1] = temp_weight;
-				temp = arr[j];
+				temp = values[j];
 				values[j] = values[j + 1];
 				values[j + 1] = temp;
 			}
@@ -59,7 +60,11 @@ void calc_value_by_weight(int values[], int weights[], float val_to_weights[], i
 int main(){
 	int values[SIZE] =  {12,14,16,77,34,16,46,42,81,21};
 	int weights[SIZE] = {14,16,77,34,16,46,42,81,21,12};
+	int tote_bag_capacity = 0;
 	float val_to_weight[SIZE];
+	int weight_split_at_end = 0;
+	int last_fraction = -1;
+	int last_weight=-1;
 	calc_value_by_weight(values, weights, val_to_weight, SIZE);
 	sort_array(val_to_weight, values, weights, SIZE);
 DE	printf("values:\t");	
@@ -74,6 +79,29 @@ DE	}
 DE	printf("\n");
 DE	for (int i=0; i<SIZE; i++){
 DE		printf("%f, ", val_to_weight[i]);
+DE	}
+DE	printf("\n");
+DE	printf("array printed\n");
+DE	printf("[Tote Bag]: Total capacity is %d\n", TOTE_SIZE);
+DE	printf("[Tote Bag]: Capacity at %d\n", tote_bag_capacity);
+	for(int i=SIZE-1; i>=0 && tote_bag_capacity<=TOTE_SIZE; i--){
+		if (weights[i]<(TOTE_SIZE-tote_bag_capacity)){
+			tote_bag_capacity+=weights[i];
+			weights[i]=0;
+DE			printf("[Tote Bag]: Capacity at %d\n", tote_bag_capacity);
+		}else{
+			last_fraction=TOTE_SIZE-tote_bag_capacity;
+			tote_bag_capacity+=(TOTE_SIZE-tote_bag_capacity);
+			weight_split_at_end=weights[i]-last_fraction;
+			weights[i]=weights[i]-last_fraction;
+DE			printf("[Tote Bag]: Capacity at %d\n", tote_bag_capacity);
+			break;
+		}
+	}
+	printf("[Tote Bag]: Tote bag is filled\n");
+DE	printf("remaining weight:\t");	
+DE	for (int i=0; i<SIZE; i++){
+DE		printf("%d, ", weights[i]);
 DE	}
 DE	printf("\n");
 //	perform_greedy_knapsack_problem(values, weights, val_to_weight);
